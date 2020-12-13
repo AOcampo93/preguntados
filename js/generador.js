0e0f97161c210df7c99038f59b897c6c8ddb052f
ArturoOcampo93 <54147798+ArturoOcampo93@users.noreply.github.com>
2020-12-13 14:24:45 -0600

    JSON

---
+++ js/generador.js
@@ -0,0 +1,10 @@
+// CALLING THE JSON
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+fetch(requestURL)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(jsonObject) {
+        console.table(jsonObject);
+        myobj = jsonObject;
+    });
